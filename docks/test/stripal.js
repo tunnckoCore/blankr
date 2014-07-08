86de425be203fb93b215fbf671eba36f565aff5b
Charlike Mike Reagent <mameto_100@mail.bg>
2014-07-08 11:20:25 +0300

    delete test, not need for now

--- docks/test/stripal.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const strip = require('../stripster')
-
-function read(src) {
-  var str = fs.readFileSync('./' + src,'utf-8');
-  return str;
-}
-
-console.log(strip(read('test/fixtures/strip-all.js')))
